ab061d292506c1db104623bc0fb7b383e1f05147
Alihan AÇIKGÖZ <alihanacikgoz1@hotmail.com>
2022-10-13 15:44:38 +0300

    First initiation completed.

--- MachineLearningImageRecognition/ViewController.swift
+++ MachineLearningImageRecognition/ViewController.swift
@@ -6,14 +6,41 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    var chosenImage = CIImage()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func changeButtonClicked(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        present(picker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true)
+        
+        if let ciImage = CIImage(image: imageView.image!) {
+            self.chosenImage = ciImage
+        }
+        recognizeImage(image: chosenImage)
+        
+    }
+    
+    func recognizeImage (image:CIImage) {
+        
+        
+    }
+    
 }
 
